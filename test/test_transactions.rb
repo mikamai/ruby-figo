bee1022c4be4760b6510840ff102273775a2304f
Lauris Z <lauris.zintzmeyer@gmail.com>
2019-08-30 15:35:19 +0200

    Revert "modif test securities"

--- test/test_transactions.rb
+++ test/test_transactions.rb
@@ -25,54 +25,63 @@ require "minitest/autorun"
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative "../lib/figo"
-require_relative 'setup'
 
 class FigoTest < MiniTest::Unit::TestCase
-  include Setup
+  def setup
+    @sut = Figo::Session.new(CONFIG["ACCESS_TOKEN"])
+  end
 
   ##   Transactions
   # Retrieve a Transaction
   def test_retreive_a_transaction
-    transactions = figo_session.get_account("A1.2").transactions
-    transaction = figo_session.get_transaction("A1.2", transactions[0].transaction_id)
+    transactions = @sut.get_account("A1.2").transactions
+    transaction = @sut.get_transaction("A1.2", transactions[0].transaction_id)
 
     assert transaction
   end
   # Retrieve Transactions of one Account
   def test_retreive_transactions_of_one_account
-    transactions = figo_session.get_account("A1.2").transactions
+    transactions = @sut.get_account("A1.2").transactions
     assert transactions.length > 0
   end
 
   # Retrieve Transactions of all Accounts
   def test_retreive_transactions_of_all_accounts
-    transactions = figo_session.transactions
+    transactions = @sut.transactions
     assert transactions.length > 0
   end
 
   # Modify a Transaction*
   def test_modify_a_transaction
     tID = @sut.get_account("A1.2").transactions[-1].transaction_id
-    exception = assert_raises(Figo::Error) {figo_session.modify_transaction("A1.2", tID, true) }
-    assert "Missing, invalid or expired access token.", exception.message
+    execption = assert_raises(Figo::Error) {@sut.modify_transaction("A1.2", tID, true) }
+    assert "Missing, invalid or expired access token.", execption.message
+
+    # @sut.modify_transaction("A1.2", tID, true)
+    #
+    # assert @sut.get_transaction("A1.2", tID)
   end
 
   # Modify all Transactions of one Account*
   def test_modify_all_transactions_of_one_account
-    exception = assert_raises(Figo::Error) { figo_session.modify_transactions(false, "A1.2") }
-    assert "Missing, invalid or expired access token.", exception.message
+    execption = assert_raises(Figo::Error) { @sut.modify_transactions(false, "A1.2") }
+    assert "Missing, invalid or expired access token.", execption.message
+    # assert_nil @sut.modify_transactions(false, "A1.2")
   end
 
   # Modify all Transactions of all Accounts*
   def test_modify_all_transactions_of_all_accounts
-    exception = assert_raises(Figo::Error) { figo_session.modify_transactions(false) }
-    assert "Missing, invalid or expired access token.", exception.message
+    execption = assert_raises(Figo::Error) { @sut.modify_transactions(false) }
+    assert "Missing, invalid or expired access token.", execption.message
+    # assert @sut.modify_transactions(false)
   end
 
   # Delete a Transaction
   def test_delete_transaction
-    tID = figo_session.get_account("A1.2").transactions[-1].transaction_id
-    exception = assert_raises(Figo::Error) { figo_session.modify_transactions("A1.2", tID) }
-    assert "Missing, invalid or expired access token.", exception.message
+    tID = @sut.get_account("A1.2").transactions[-1].transaction_id
+
+    execption = assert_raises(Figo::Error) { @sut.modify_transactions("A1.2", tID) }
+    assert "Missing, invalid or expired access token.", execption.message
+    # assert @sut.modify_transactions("A1.2", tID)
   end
 end
